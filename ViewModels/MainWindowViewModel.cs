143fc9f1691303099cdc9ffb1aa8f39df9bb24a3
Timothy Parez <timothyparez@outlook.com>
2020-02-15 15:35:26 +0800

    Initial commit

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace TreeViewIssue.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<string> Items {get; set;} = new ObservableCollection<string>();
+                
+        [Reactive]
+        public string SelectedItem {get; set;}
+        public MainWindowViewModel()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Items.Add($"Item {i:##}");                
+            }
+            
+            this.WhenAnyValue(x => x.SelectedItem)
+                .Subscribe(x => Console.WriteLine($"Changed: {x ?? "null"}"));
+        }      
+    }
+}
